b09e8863c0a517cb97a81e721626cb0abbe89769
choiahhyeon <choiahye0n211@gmail.com>
2026-04-03 18:57:59 +0900

    feat: 상품 가격 표시 업데이트

--- CH2/src/components/ProductCard.jsx
+++ CH2/src/components/ProductCard.jsx
@@ -10,7 +10,7 @@ function ProductCard ({image, brand, name, price, discountRate, isLiked}) {
         <div className={"product-details"}>
           <p className={"product-brand"}>{brand}</p>
           <p className={"product-name"}>{name}</p>
-          <p className={"product-price"}>{discountRate && <span className={"discountRate"}>{discountRate}%</span>} {price}</p>
+          <p className={"product-price"}>{discountRate && <span className={"discountRate"}>{discountRate}%</span>} {price.toLocaleString()}</p>
         </div>
         <img className={"like-img"} src={isLiked ? Heart_active : Heart} alt="좋아요" />
         </div>
